b036f5bcd1e91dd9f0d4d1a723cd4b91c9d5c349
Varun Kumar <vkvarun04712@gmail.com>
2025-08-14 00:07:48 +0530

    Add files via upload

---
+++ looping.java
@@ -0,0 +1,21 @@
+public class Main{
+    public static void main(String[] args){
+//        for (int i=0 ; i<=10 ; i++){
+//
+//            System.out.println();
+//        }
+
+//        int i = 1;
+//        while(i<=20){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        int i = 40;
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while(i>10);
+    }
+}
